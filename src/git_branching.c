b063deabf7f5be3752885522205cd5e4cffe67e0
Bartos <b.barcinski@gmail.com>
2018-05-02 18:37:28 +0200

    added final change to r2_comments

--- src/git_branching.c
+++ src/git_branching.c
@@ -16,5 +16,6 @@ int main(void) {
 	/*here is our first code*/
 	/*here is first change on r2_contacts branch*/
 	/*here is bug fixing on master branch */
+	/*here is final change on r2_contacts branch*/
 	return EXIT_SUCCESS;
 }
